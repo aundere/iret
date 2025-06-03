ab9e47e6b950b26f75a538db001c5dacfb2a1b37
aundere <58791453+aundere@users.noreply.github.com>
2025-06-03 08:09:48 +0200

    Implement noise generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::io::{Stdout, Write};
+
+use crossterm::{cursor::MoveTo, terminal::size, ExecutableCommand};
+use fastnoise_lite::FastNoiseLite;
+
+const BRIGHTNESS: [u8; 5] = [ b' ', b'.', b'-', b'=', b'#'];
+
+fn draw_noise(stdout: &mut Stdout, noise: &FastNoiseLite, offset: f32) {
+    let size = size().unwrap();
+
+    stdout.execute(MoveTo(0, 0)).unwrap();
+
+    for y in 0..size.1 {
+        for x in 0..size.0 {
+            let value = (noise.get_noise_2d(x as f32, y as f32 + offset) + 1.0) / 2.0;
+            let index = (value * BRIGHTNESS.len() as f32) as usize;
+
+            stdout.write(&[BRIGHTNESS[index]]).unwrap();
+        }
+    }
+}
+
 fn main() {
-    println!("meow");
+    let mut noise = FastNoiseLite::new();
+    noise.set_noise_type(Some(fastnoise_lite::NoiseType::OpenSimplex2S));
+
+    let mut stdout = std::io::stdout();
+
+    crossterm::terminal::enable_raw_mode().unwrap();
+
+    let mut frame_times = Vec::new();
+
+    for x in 0..100 {
+        let start = std::time::Instant::now();
+
+        draw_noise(&mut stdout, &noise, x as f32);
+
+        let elapsed = start.elapsed();
+        frame_times.push(elapsed);
+
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+
+    crossterm::terminal::disable_raw_mode().unwrap();
+
+    println!("Average frame time: {:?}", frame_times.iter().sum::<std::time::Duration>() / frame_times.len() as u32);
 }
